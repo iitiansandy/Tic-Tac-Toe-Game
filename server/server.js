9aa2e10d8e77d74e1b82ad62a6e07078bf10c78e
iitiansandy <sandeep.sid.kumar10@gmail.com>
2023-02-16 18:48:31 +0530

    changes done

---
+++ server/server.js
@@ -0,0 +1,146 @@
+const WebSocket = require('ws');
+
+// create a new WebSocket server on port 8080
+const server = new WebSocket.Server({ port: 5050 });
+
+let game = null;
+let player1 = null;
+let player2 = null;
+
+// Listen for WebSocket connections
+server.on('connection', (socket) => {
+    // If this is the first player to connect, assign them as player1;
+    if (!player1) {
+        player1 = socket;
+        console.log('Player 1 connected');
+        socket.send('You are player 1');
+    } else if (!player2) {
+        // If this is the second player to connect, assign them as player2;
+        player2 = socket;
+        console.log('Player 2 connected');
+        socket.send('You are player 2');
+
+        // start a new game
+        game = new Game(player1, player2);
+        game.start();
+    } else {
+        // if there are already two players connected, reject the connection
+        console.log('Connection rejected - game is full');
+        socket.send('Game is full');
+        socket.close();
+    }
+
+    // Listen for messages form the players
+    socket.on('message', (message) => {
+        // pass the message to the game
+        game.handleMessage(socket, message);
+    });
+
+    // Lesten for disconnections
+    socket.on('close', () => {
+        if (socket === player1) {
+            console.log('Player 1 disconnected');
+            player1 = null;
+        } else if (socket === player2) {
+            console.log('Player 2 disconnected');
+            player2 = null;
+        }
+
+        // End the game if a player get disconnected
+        if (game) {
+            game.end();
+            game = null;
+        }
+    });
+});
+
+
+// Define the Tic-Tac-Toe game class
+class Game {
+    constructor(player1, player2) {
+        this.board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']];
+        this.currentPlayer = player1;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    start() {
+        // Send the initial board state to both players
+        this.player1.send(`START ${this.serializeBoard()}`);
+        this.player2.send(`START ${this.serializeBoard()}`);
+    }
+
+    end() {
+        // Notify both players that the game has ended
+        this.player1.send('END');
+        this.player2.send('END');
+    }
+
+    handleMessage(player, message) {
+        // Ignore messages from players who are not part of the game
+        if (player !== this.currentPlayer) {
+            return;
+        }
+
+        const [command, ...args] = message //.split(' ');
+
+        switch (command) {
+            case 'MOVE':
+                // Parse the row and column arguments
+                const [row, col] = args.map((arg) => parseInt(arg));
+
+                // Check if the move is valid
+                if (this.board[row][col] !== '_') {
+                    player.send(`INVALID MOVE ${row} ${col}`);
+                    return;
+                }
+
+                // Make the move and switch to the other player
+                this.board[row][col] = player === this.player1 ? 'X' : 'O';
+                this.currentPlayer = player === this.player1 ? this.player2 : this.player1;
+
+                // Check if the game has ended
+                if (this.checkWin()) {
+                    this.end();
+                } else if (this.checkTie()) {
+                    this.end();
+                } else {
+                    // Notify both players of the new board state and whose turn it is
+                    const boardState = this.serializeBoard();
+                    this.player1.send(`UPDATE ${boardState} ${this.currentPlayer === this.player1}`);
+                    this.player2.send(`UPDATE ${boardState} ${this.currentPlayer === this.player2}`);
+                }
+                break;
+            default:
+                player.send(`UNKNOWN COMMAND ${command}`);
+                break;
+        }
+    }
+    serializeBoard() {
+        return this.board.map((row) => row.join('')).join('');
+    }
+
+    checkWin() {
+        // Check rows
+        for (let row = 0; row<3; row++) {
+            if (this.board[row][0] !== '_' && this.board[row][0] === this.board[row][1] && this.board[row][0] === this.board[row][2]) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (this.board[0][0] !== '_' && this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2]) {
+            return true;
+        }
+
+        if (this.board[0][2] !== '_' && this.board[0][2] === this.board[1][1] && this.board[0][2] === this.board[2][0]) {
+            return true;
+        }
+
+        return false;
+    }
+
+    checkTie() {
+        return this.board.every((row) => row.every((cell) => cell !== '_'));
+    }
+};
